7e8cafc18b8b16fd8fa1131811d5a46a1e308800
Lauren Hoock <laurenh8916@gmail.com>
2019-11-27 19:43:07 -0700

    repeat string test solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,value) {
+    let userString = string;
+    let repeatVal = value;
+    let repeatedString = "";
 
+    if(repeatVal < 0) {
+        return "ERROR";
+    }
+
+    for(let i = 0; i < repeatVal; i++) {
+        repeatedString += userString;
+    }
+
+    return repeatedString;
 }
 
 module.exports = repeatString
