92313aab13ea88d441930bea1a85867d827d9852
Lauren Hoock <laurenh8916@gmail.com>
2019-11-27 20:00:17 -0700

    added reverseString test solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let userString = string;
+    let splitString = userString.split("");
+    let reversedString = [];
 
+    for(let i = 0; i < userString.length; i++) {
+        let letter = splitString.pop();
+        reversedString.push(letter);
+    }
+
+    return reversedString.join("");
 }
 
 module.exports = reverseString
