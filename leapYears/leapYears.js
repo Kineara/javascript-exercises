9b684124db00a0d999958681e0e3c787985cbb24
Lauren Hoock <laurenh8916@gmail.com>
2019-11-29 09:11:59 -0700

    added leapYear solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    let year = num;
 
+    if((year%4 == 0) && ((year%100 == 0) ? (year%400 == 0 ? true : false) : true)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = leapYears
