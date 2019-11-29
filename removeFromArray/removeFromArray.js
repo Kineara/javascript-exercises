b9f4827ef5e8aff8676db0d441b952e14974ddcb
Lauren Hoock <laurenh8916@gmail.com>
2019-11-29 08:36:27 -0700

    added removeFromArray solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...userArgs) {
+    let userArray = array;
+    let removeValues = userArgs;
+
+    for(let i = 0; i < removeValues.length; i++) {
+        for(let x = 0; x < userArray.length; x++) {
+            if(userArray[x] === removeValues[i]) {
+                userArray.splice(x, 1);
+            }
+        }
+    }
+
+    return userArray;
 
 }
 
