66589aead96fbeace0ce5e7520880f840a6e0cea
Lauren Hoock <laurenh8916@gmail.com>
2019-11-29 08:52:47 -0700

    added sumAll solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let firstInt, lastInt;
+    if(a<0 || b<0 || typeof a != 'number' || typeof b != 'number') {
+        return "ERROR";
+    }
+    else if(a>b) {
+        firstInt = b;
+        lastInt = a;
+    } else {
+        firstInt = a;
+        lastInt = b;
+    }
+    let sum = 0;
+
+    for(let i = firstInt; i <= lastInt; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 }
 
