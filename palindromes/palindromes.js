902b6ca51cbaa2592b4644d4b5a3c4e9cfca9e39
Lauren Hoock <laurenh8916@gmail.com>
2020-02-18 06:24:54 -0700

    palindromes solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
-}
+const palindromes = function(inputString) {
+    let splitString = inputString.toLowerCase()
+                        .split('')
+                        .filter(letter => "abcdefghijklmnopqrstuvwxyz".includes(letter));
+    let origString = splitString.join("");
+    let testString = splitString.reverse().join("");
+    return origString === testString;
+    }
 
 module.exports = palindromes
