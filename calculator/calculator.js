7de21a6c2882003a93a21310658dcfd71edaccb9
Lauren Hoock <laurenh8916@gmail.com>
2020-02-17 23:18:29 -0700

    calculator solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
 function add () {
-	
+	return 0 + arguments[0] + arguments[1];
 }
 
 function subtract () {
-	
+	return arguments[0] - arguments[1];
 }
 
 function sum () {
-	
+	const numList = Array.from(...arguments);
+	const sum = numList.reduce((total, num) => {
+		return total + num;
+	}, 0);
+	return sum;
 }
 
 function multiply () {
-	
+	const numList = Array.from(...arguments);
+	const product = numList.reduce((total,num) => {
+		return total *= num;
+	}, 1);
+	return product;
 }
 
 function power() {
-	
+	return Math.pow(arguments[0], arguments[1]);
 }
 
 function factorial() {
-	
+	let num = arguments[0];
+	if (num == 0 || num == 1)
+		return 1;
+	let result = num;
+	while (num > 1) {
+		num --;
+		result *= num;
+	}
+	return result;
 }
 
 module.exports = {
