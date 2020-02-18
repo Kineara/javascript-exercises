a720caf04ee9a05d942e8c1edef533676fb71d09
Lauren Hoock <laurenh8916@gmail.com>
2020-02-18 13:58:38 -0700

    partial caesar solution

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,15 @@
-const caesar = function() {
-
-}
+const caesar = function(string,shift) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let codedString = '';
+    inputString = Array.from(string);
+    // Get alphabetical index of each letter in the inputString array
+    // Subtract the shift factor
+    // Use new index to get new letter from alphabet
+    // Concatenate letter to codedString
+    indexes = inputString.map(function (letter) {
+        codedString += alphabet[alphabet.indexOf(letter) - shift];
+    })
+    console.log(codedString);
+}   
 
 module.exports = caesar
