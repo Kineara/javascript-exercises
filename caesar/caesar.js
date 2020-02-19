4aaa04ecd966a603358003bd33aa30b3466e03d9
Lauren Hoock <laurenh8916@gmail.com>
2020-02-18 18:40:50 -0700

    the long awaited caesar solution

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -4,25 +4,28 @@ const caesar = function(string,shift) {
     let codedString = '';
     let inputString = Array.from(string);
 
-    // save capital letter indexes
     const capitalLetters = inputString.map(letter => alphabetCaps.includes(letter) ? 1 : 0);   
 
     indexes = inputString.map(function (letter) {
         if (alphabetLower.includes(letter) || alphabetCaps.includes(letter)) {
-        return alphabetLower.indexOf(letter.toLowerCase()) - shift
+            indexTest = alphabetLower.indexOf(letter.toLowerCase()) + shift;
+            while(indexTest < 0 || indexTest > 25) {
+                if(indexTest < 0) indexTest = indexTest + 26;
+                else indexTest = indexTest % 26;
+            }
+            return indexTest;
         } else return letter;
     })
-    console.log(indexes);
 
     for (let i=0; i<capitalLetters.length; i++) {
-        if (!isNaN(indexes[i]) && (indexes[i] !== " ")) {
-        capitalLetters[i] === 1
-        ? codedString += alphabetCaps[indexes[i]]
-        : codedString += alphabetLower[indexes[i]];
+        if (typeof indexes[i] === "number") {
+            capitalLetters[i] === 1
+            ? codedString += alphabetCaps[indexes[i]]
+            : codedString += alphabetLower[indexes[i]];
         } else codedString += indexes[i];
     }
     
-    console.log(codedString);
+    return codedString;
 }
 
 module.exports = caesar
