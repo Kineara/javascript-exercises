622c12aae3c365c05d7fdfd1204c8104aeb488fa
Lauren Hoock <laurenh8916@gmail.com>
2020-02-23 22:19:28 -0700

    findTheOldest solution

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
 
+    function zombieCheck(deathYear) {
+        let currentYear = new Date().getFullYear();
+
+        return !deathYear ? currentYear : deathYear;
+    }
+    
+    const oldest = people.sort((a,b) => ((zombieCheck(a.yearOfDeath) - a.yearOfBirth) < (zombieCheck(b.yearOfDeath) - b.yearOfBirth)) ? 1 : -1);
+
+    return oldest[0];
 }
 
 module.exports = findTheOldest
