17b2a15e1360084a64adb0550be0fc6e16de8622
Lauren Hoock <laurenh8916@gmail.com>
2020-02-23 22:21:54 -0700

    findTheOldest cleanup

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,9 +1,7 @@
 let findTheOldest = function(people) {
-
+    
     function zombieCheck(deathYear) {
-        let currentYear = new Date().getFullYear();
-
-        return !deathYear ? currentYear : deathYear;
+        return !deathYear ? new Date().getFullYear() : deathYear;
     }
     
     const oldest = people.sort((a,b) => ((zombieCheck(a.yearOfDeath) - a.yearOfBirth) < (zombieCheck(b.yearOfDeath) - b.yearOfBirth)) ? 1 : -1);
